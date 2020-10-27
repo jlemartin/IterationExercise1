4f41a5a3f91a01e797839bf71d97d5be1c26bb4c
John Martin <jlemartin7@gmail.com>
2020-10-27 14:54:16 -0500

    Part 1 of Exercise

---
+++ IterationExercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IterationExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
